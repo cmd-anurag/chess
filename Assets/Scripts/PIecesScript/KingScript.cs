cc1a4e37c3754dd11aaea2adbbe7ca83769390a0
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-08-29 16:20:00 +0530

    Implemented king moves

--- Assets/Scripts/PIecesScript/KingScript.cs
+++ Assets/Scripts/PIecesScript/KingScript.cs
@@ -13,26 +13,31 @@ public class KingScript : PieceBase
     }
     public override List<string> GenerateMoves(GameObject piece, GameObject currentSquare) {
         List<string> legalMoves = new();
-        (int, int)[] directions = {(1, 1), (1, -1), (-1, 1), (-1, -1)};
+        (int, int)[] directions = {(1, 1), (1, -1), (-1, 1), (-1, -1), (1, 0), (-1, 0), (0, 1), (0, -1)};
         SquareScript squareScript = currentSquare.GetComponent<SquareScript>();
-        char file = squareScript.file;
-        int rank = squareScript.rank;
+        char file;
+        int rank;
 
         foreach ((int fileDir, int rankDir) in directions) {
-            while(true) {
-                file += (char)fileDir;
-                rank += rankDir;
-                GameObject newSquare = boardScript.GetSquareAt(file, rank);
-                if(newSquare==null) {
-                    break;
-                }
-                SquareScript newSquareScript = newSquare.GetComponent<SquareScript>();
-                if(newSquareScript.occupiedBy != null) {
-                    legalMoves.Add(newSquare.name);
-                    break;
-                }
-                legalMoves.Add(newSquare.name);
+            file = squareScript.file;
+            rank = squareScript.rank;
+            file += (char)fileDir;
+            rank += rankDir;
+            GameObject newSquare = boardScript.GetSquareAt(file, rank);
+            if(newSquare==null) {
+                continue;
             }
+            SquareScript newSquareScript = newSquare.GetComponent<SquareScript>();
+            if(newSquareScript.occupiedBy != null) {
+                
+                gameLogicManagerScript.HighlightCapture(newSquare);
+                legalMoves.Add(newSquare.name);
+                continue;
+            }
+            gameLogicManagerScript.HighLightSquare(newSquare);
+            legalMoves.Add(newSquare.name);
+            file = squareScript.file;
+            rank = squareScript.rank;
         }
 
         return legalMoves;
